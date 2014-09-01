bd8355a86c68ace6793b30ceb2336f25dfab7bb6
David O'Trakoun <me@davidosomething.com>
2014-09-01 19:19:51 -0400

    add bugsnag

--- functions.php
+++ functions.php
@@ -8,6 +8,22 @@ wp_enqueue_script('glutton-main',
 	get_stylesheet_directory_uri() . '/static/app/app.js'
 );
 
+
+/**
+ * glutton_bugsnag
+ *
+ * Echo out the bugsnag snippet for JS error tracking
+ */
+function glutton_bugsnag() {
+	echo <<<SNIPPET
+	<script
+		src="//d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js"
+		data-apikey="9fd6c80ccab8cc199fc428d5ecfb83b1">
+	</script>
+SNIPPET;
+}
+add_action( 'wp_print_head_scripts', 'glutton_bugsnag' );
+
 /**
  * glutton_jsmonitor
  *
@@ -25,3 +41,4 @@ function glutton_jsmonitor() {
 SNIPPET;
 }
 add_action( 'wp_print_footer_scripts', 'glutton_jsmonitor' );
+
